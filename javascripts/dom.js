df5dfd4152fbf07da57be70a031cf457896e9e38
Xuehui Chen <jeffreychen2016@gmail.com>
2018-05-28 15:08:43 -0500

    added zipcode validation functions

--- javascripts/dom.js
+++ javascripts/dom.js
@@ -3,7 +3,7 @@ const printWidgets = (weatherData,days) => {
   strang += `<div class="row margin-top">`;
 
   if (days === 1) {
-    strang += `<div class="col-sm-6 col-md-4 col-md-offset-4 weathercard-current-day">`;
+    strang += `<div class="col-sm-6 col-md-4 col-md-offset-4 weathercard-current-day weather-widgets">`;
     strang += `  <div class="thumbnail">`;
     // strang += `    <img src="..." alt="...">`;
     strang += `    <div class="caption">`;
@@ -20,7 +20,7 @@ const printWidgets = (weatherData,days) => {
     weatherData.list.forEach((weather) => {
       const hour = weather.dt_txt.slice(11,13);
       if (hour === '00') {
-        strang += `<div class="col-sm-6 col-md-4 weathercard-other-than-current-day">`;
+        strang += `<div class="col-sm-6 col-md-4 weathercard-other-than-current-day weather-widgets">`;
         strang += `  <div class="thumbnail">`;
         // strang += `    <img src="..." alt="...">`;
         strang += `    <div class="caption">`;
@@ -41,48 +41,11 @@ const printWidgets = (weatherData,days) => {
   $(`.weathercard-other-than-current-day:gt(${days - 1})`).remove();
 };
 
-// {
-//   "coord": {
-//       "lon": -86.72,
-//       "lat": 36.07
-//   },
-//   "weather": [
-//       {
-//           "id": 701,
-//           "main": "Mist",
-//           "description": "mist",
-//           "icon": "50d"
-//       }
-//   ],
-//   "base": "stations",
-//   "main": {
-//       "temp": 77.43,
-//       "pressure": 1014,
-//       "humidity": 65,
-//       "temp_min": 75.2,
-//       "temp_max": 80.6
-//   },
-//   "visibility": 16093,
-//   "wind": {
-//       "speed": 3.36
-//   },
-//   "clouds": {
-//       "all": 75
-//   },
-//   "dt": 1527351300,
-//   "sys": {
-//       "type": 1,
-//       "id": 2532,
-//       "message": 0.0052,
-//       "country": "US",
-//       "sunrise": 1527330808,
-//       "sunset": 1527382496
-//   },
-//   "id": 420033672,
-//   "name": "Nashville",
-//   "cod": 200
-// }
+const removeWidgets = () => {
+  $('.weather-widgets').remove();
+};
 
 module.exports = {
   printWidgets,
+  removeWidgets,
 };
