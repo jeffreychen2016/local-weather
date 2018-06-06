9ed2daef5ee9afae419eaa4da1e0545c501a0b92
Xuehui Chen <jeffreychen2016@gmail.com>
2018-06-05 21:20:46 -0500

    added function to only display authentication button in the nav before user is not logged in. Display all buttons except for authentication button when user is logged in

--- javascripts/auth.js
+++ javascripts/auth.js
@@ -6,12 +6,18 @@ const checkLoginStatus = () => {
     if (user) {
       // User is signed in.
       // move it from log in event *******
+      $('#savedWeatherBtn,#searchBtn,#logoutBtn').removeClass('hide');
+      $('#authBtn').addClass('hide');
+
       $('#savedWeather').removeClass('hide');
       $('#authentication').addClass('hide');
       events.getAllSavedWeather();
     } else {
       // No user is signed in.
       // move it from log out event ------
+      $('#savedWeatherBtn,#searchBtn,#logoutBtn').addClass('hide');
+      $('#authBtn').removeClass('hide');
+
       $('#authentication').removeClass('hide');
       $('#savedWeather').addClass('hide');
     };
